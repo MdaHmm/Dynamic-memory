9f8c3ec3eaa749c8db8e1a4b0a3c81f7f58dc9e3
Academy_ekat@itstep.academy <Academy_ekat@itstep.academy>
2023-03-20 18:51:49 +0500

    Салам алекум

--- Green_Project/Source.cpp
+++ Green_Project/Source.cpp
@@ -8,7 +8,10 @@ int main() {
 	// Память - статическая (стэк)
 	//			динамическая (куча)
 
+	int* pointer = new int; //  Выделение динамической памяти размером 4 байта
 
+	*pointer = 7;
+	std::cout << "*pointer = " << *pointer << std::endl;
 
 
 
