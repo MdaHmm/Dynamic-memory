cff0c52598242c2ebe1f76c6d2086070ec30093a
Academy_ekat@itstep.academy <Academy_ekat@itstep.academy>
2023-03-20 18:40:28 +0500

    Add project files.

---
+++ Green_Project/Source.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int n;
+
+	// Память - статическая (стэк)
+	//			динамическая (куча)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
